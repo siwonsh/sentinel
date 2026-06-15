2a377fea4478f3a57d16e8b56b175a745a639e11
Siwon <siwon.shins@gmail.com>
2026-06-15 11:58:40 +0200

    CP2

---
+++ Sentinel/Services/IEquipmentService.cs
@@ -0,0 +1,10 @@
+﻿using Sentinel.Models;
+
+namespace Sentinel.Services;
+
+public interface IEquipmentService
+{
+    public Task<IReadOnlyList<EquipmentUnit>> GetFleetAsync(CancellationToken cancellationToken = default);
+
+    public event EventHandler<TelemetryChangedEventArgs> TelemetryChanged;
+}
---
+++ Sentinel/Services/TelemetryChangedEventArgs.cs
@@ -0,0 +1,5 @@
+﻿namespace Sentinel.Services;
+
+public class TelemetryChangedEventArgs : EventArgs
+{
+}
